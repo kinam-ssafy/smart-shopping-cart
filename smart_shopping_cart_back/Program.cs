256e9dfd967edc8b6e6c97dac379a37b3bedff5a
나성현 <indiana2@naver.com>
2026-02-02 17:20:37 +0900

    초기 저장용

--- smart_shopping_cart_back/Program.cs
+++ smart_shopping_cart_back/Program.cs
@@ -3,12 +3,14 @@ using smart_shopping_cart_back.Services;
 using smart_shopping_cart_back.Data;
 using Microsoft.EntityFrameworkCore;
 
+
+DotNetEnv.Env.Load();
 var builder = WebApplication.CreateBuilder(args);
 
 // ===== 서비스 등록 =====
 builder.Services.AddControllers();
 builder.Services.AddCorsPolicy();      // CORS 설정
-// builder.Services.AddMqttServices();    // MQTT 서비스 + 백그라운드 서비스
+builder.Services.AddMqttServices();    // MQTT 서비스 + 백그라운드 서비스(연결로직 제외)
 builder.Services.AddSseServices();     // SSE 서비스
 builder.Services.AddSearchServices();  // 검색 서비스
 
