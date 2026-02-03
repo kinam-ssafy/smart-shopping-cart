649bc42cd0ae6f0309ce411f357aefd3bb2a7e2e
강병구 <0102kbgblue@naver.com>
2026-02-03 13:45:20 +0900

    feat(stm): add a401_stm32_controller

---
+++ stm/a401_stm32_control/Core/Inc/u2_log.h
@@ -0,0 +1,13 @@
+/*
+ * u2_log.h
+ *
+ *  Created on: Feb 3, 2026
+ *      Author: SSAFY
+ */
+
+#ifndef INC_U2_LOG_H_
+#define INC_U2_LOG_H_
+
+
+
+#endif /* INC_U2_LOG_H_ */
---
+++ stm/a401_stm32_control/Core/Src/u2_log.c
@@ -0,0 +1,8 @@
+/*
+ * u2_log.c
+ *
+ *  Created on: Feb 3, 2026
+ *      Author: SSAFY
+ */
+
+
